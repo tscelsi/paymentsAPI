1551e9d267e75825e3e374a83290d87e6924699c
tscelsi <tscelsi@student.unimelb.edu.au>
2022-03-18 15:42:12 +1100

    middleware: added to router

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import paymentRouter from './payment-router';
-
+import { validateUser } from 'src/middleware/user';
 
 // Export the base-router
 const baseRouter = Router();
 
 // Setup routers
-baseRouter.use('/payments/', paymentRouter);
+baseRouter.use('/payments/', validateUser, paymentRouter);
 
 // Export default.
 export default baseRouter;
